a7b95cea47dc80636b97b813a50b1cca8c029b64
Cyril Feraudet <cyril@feraudet.com>
2019-09-30 17:17:38 +0200

    Fix missing braces

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ resource "aws_route53_record" "this" {
   name            = aws_acm_certificate.this.domain_validation_options[count.index]["resource_record_name"]
   type            = aws_acm_certificate.this.domain_validation_options[count.index]["resource_record_type"]
   zone_id         = element(var.zone_ids, count.index)
-  records = aws_acm_certificate.this.domain_validation_options[count.index]["resource_record_value"]
-  ttl     = 60
+  records         = [aws_acm_certificate.this.domain_validation_options[count.index]["resource_record_value"]]
+  ttl             = 60
 }
 
 resource "aws_acm_certificate_validation" "this" {
